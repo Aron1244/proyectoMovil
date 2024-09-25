202c98fa2ca3244f03583caa0aad8da163a9665e
Benjamin Herrera <raycuasa80@gmail.com>
2024-09-25 14:07:45 -0300

    Se creo un page 404 custom

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './guards/auth.guard';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 const routes: Routes = [
   {
@@ -49,6 +50,7 @@ const routes: Routes = [
       ),
     canActivate: [AuthGuard],
   },
+  { path: '**', component: NotFoundComponent },
 ];
 
 @NgModule({
