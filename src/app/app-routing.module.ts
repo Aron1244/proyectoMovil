a83211ede89bd79994b8749ff7949db0716ff572
Benjamin Herrera <raycuasa80@gmail.com>
2024-09-25 13:42:55 -0300

    Se implente guard en el proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   {
     path: 'home',
     loadChildren: () =>
       import('./pages/home/home.module').then((m) => m.HomePageModule),
+    canActivate: [AuthGuard],
   },
   {
     path: '',
@@ -20,23 +22,33 @@ const routes: Routes = [
   {
     path: 'user-info',
     loadChildren: () =>
-      import('./pages/user-info/user-info.module').then((m) => m.UserInfoPageModule),
+      import('./pages/user-info/user-info.module').then(
+        (m) => m.UserInfoPageModule
+      ),
+    canActivate: [AuthGuard],
   },
   {
     path: 'clases',
     loadChildren: () =>
       import('./pages/clases/clases.module').then((m) => m.ClasesPageModule),
+    canActivate: [AuthGuard],
   },
   {
     path: 'historial',
     loadChildren: () =>
-      import('./pages/historial/historial.module').then((m) => m.HistorialPageModule),
+      import('./pages/historial/historial.module').then(
+        (m) => m.HistorialPageModule
+      ),
+    canActivate: [AuthGuard],
   },
   {
     path: 'rest-pass',
-    loadChildren: () => import('./pages/rest-pass/rest-pass.module').then( m => m.RestPassPageModule)
+    loadChildren: () =>
+      import('./pages/rest-pass/rest-pass.module').then(
+        (m) => m.RestPassPageModule
+      ),
+    canActivate: [AuthGuard],
   },
-
 ];
 
 @NgModule({
