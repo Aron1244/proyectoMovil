16f46a52482051e54e1602bff48e21b49d8c3f04
Benjamin Herrera <raycuasa80@gmail.com>
2024-10-07 16:44:46 -0300

    Se creo una api de clima en santiago

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ToastController } from '@ionic/angular';
 import { LoginService } from 'src/app/services/login.service';
+import { ClimateService } from 'src/app/services/climate.service';
 
 @Component({
   selector: 'app-home',
@@ -11,12 +12,14 @@ import { LoginService } from 'src/app/services/login.service';
 export class HomePage {
   user!: string;
   developer: String = 'ByteForge';
+  weatherData: any;
 
   constructor(
     private activeroute: ActivatedRoute,
     private router: Router,
     private toaster: ToastController,
-    private loginService: LoginService
+    private loginService: LoginService,
+    private climateService: ClimateService
   ) {
     this.activeroute.queryParams.subscribe((params) => {
       let state = this.router.getCurrentNavigation()?.extras.state;
@@ -26,8 +29,11 @@ export class HomePage {
     });
   }
 
-  async logOut() {
+  ngOnInit() {
+    this.getWeather();
+  }
 
+  async logOut() {
     this.loginService.logOut();
     this.router.navigate(['/login']);
 
@@ -39,7 +45,7 @@ export class HomePage {
     });
     toast.present();
   }
-  
+
   goToUserInfo() {
     this.router.navigate(['/user-info'], {
       state: { user: this.user },
@@ -56,5 +62,36 @@ export class HomePage {
     });
 
     toast.present();
+  }
+
+  getWeather() {
+    this.climateService.getWeather().subscribe(
+      (data) => {
+        this.weatherData = data;
+        console.log(this.weatherData); // Puedes ver los datos en la consola
+      },
+      (error) => {
+        console.error('Error al obtener el clima', error);
+      }
+    );
+  }
+
+  getWeatherIcon(condition: string): string {
+    if (
+      condition.toLowerCase().includes('sun') ||
+      condition.toLowerCase().includes('clear')
+    ) {
+      return 'sunny-outline'; // Soleado
+    } else if (condition.toLowerCase().includes('cloud')) {
+      return 'cloud-outline'; // Nublado
+    } else if (condition.toLowerCase().includes('rain')) {
+      return 'rainy-outline'; // Lluvia
+    } else if (condition.toLowerCase().includes('snow')) {
+      return 'snow-outline'; // Nieve
+    } else if (condition.toLowerCase().includes('storm')) {
+      return 'thunderstorm-outline'; // Tormenta
+    } else {
+      return 'partly-sunny-outline'; // Clima mixto o desconocido
+    }
   }
 }
