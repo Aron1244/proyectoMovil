80937c03b746c7f1fa34fc18a5af45de4c1db6be
Benjamin <raycuasa80@gmail.com>
2024-11-13 07:35:12 -0300

    Se inicializo el storage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
@@ -12,6 +12,20 @@ import { HttpClientModule } from '@angular/common/http';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { environment } from 'src/environments/environment';
+import { StorageService } from './services/storage.service';
+
+export function initApp(storageService: StorageService) {
+  return () => storageService.init();
+}
+
+export function storageService() {
+  return {
+    provide: APP_INITIALIZER,
+    useFactory: initApp,
+    deps: [StorageService],
+    multi: true,
+  };
+}
 
 @NgModule({
   declarations: [AppComponent],
@@ -26,6 +40,7 @@ import { environment } from 'src/environments/environment';
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
     provideFirestore(() => getFirestore()),
+    storageService()
   ],
   bootstrap: [AppComponent],
 })
